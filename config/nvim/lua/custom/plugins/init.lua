3c01de448d4e00fba739f5a95dca8e27ec5fc75c
Yuito <aoi.yuito.ehou@gmail.com>
2026-01-30 10:51:18 +0000

    added nvim config files

---
+++ config/nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,68 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  {
+    'yuito2k/vault.nvim',
+    config = function()
+      require('vault')
+    end,
+  },
+  {
+    'vyfor/cord.nvim',
+    build = ':Cord update',
+    -- opts = {}
+  },
+  {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    config = function()
+      require('nvim-autopairs').setup {}
+    end,
+  },
+  {
+    'nvimtools/none-ls.nvim',
+    dependencies = {
+      'nvimtools/none-ls-extras.nvim',
+      'jayp0521/mason-null-ls.nvim', -- ensure dependencies are installed
+    },
+    config = function()
+      -- list of formatters & lintersfor masontoinstall
+      require('mason-null-ls').setup {
+        ensure_installed = {
+          'ruff',
+          'prettier',
+          'shfmt',
+        },
+        automatic_installation = true,
+      }
+
+      local null_ls = require 'null-ls'
+      local sources = {
+        require('none-ls.formatting.ruff').with { extra_args = { '--extend-select', 'I' } },
+        require 'none-ls.formatting.ruff_format',
+        null_ls.builtins.formatting.prettier.with { filetypes = { 'json', 'yaml', 'markdown' } },
+        null_ls.builtins.formatting.shfmt.with { args = { '-i', '4' } },
+      }
+
+      local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
+      null_ls.setup {
+        -- debug = true, 一 Enable debug mode.Inspect Logs with:NullLsLog.
+        sources = sources,
+        on_attach = function(client, bufnr)
+          if client.supports_method 'textDocument/formatting' then
+            vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+            vim.api.nvim_create_autocmd('BufWritePre', {
+              group = augroup,
+              buffer = bufnr,
+              callback = function()
+                vim.lsp.buf.format { async = false }
+              end,
+            })
+          end
+        end,
+      }
+    end,
+  },
+}
---
+++ config/nvim/lua/kickstart/plugins/neo-tree.lua
@@ -0,0 +1,30 @@
+-- Neo-tree is a Neovim plugin to browse the file system
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  lazy = false,
+  keys = {
+    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+  },
+  opts = {
+    filesystem = {
+      window = {
+        mappings = {
+          ['\\'] = 'close_window',
+        },
+      },
+      filtered_items = {
+        visible = true, -- This setting may not be the direct control you need
+        hide_dotfiles = true, -- Set this to false to show dotfiles
+        hide_gitignored = true, -- Set this to false to show gitignored files
+      },
+    },
+  },
+}
